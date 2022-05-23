52eba5b1446f93ce022a9ee889b5051d783356cd
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-23 14:21:38 -0500

    import and resize the youtube logo

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ title = screen.title(
 )
 canvas = Canvas(screen, width=500, height= 500)
 canvas.pack()
+
+# Youtube Logo
+logo = PhotoImage(file= 'youtube_logo.png')
+# Resize the image
+logo = logo.subsample(2,2)
+
+canvas.create_image(250, 80, image = logo)
+
+screen.mainloop()
