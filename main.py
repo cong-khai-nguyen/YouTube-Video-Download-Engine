18da8cba197ed2ceb0031282a88ce735910262d5
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-23 14:11:24 -0500

    set up the Python GUI

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from tkinter import *
 from tkinter import filedialog
 
+# Set up the GUI
+screen = Tk()
+title = screen.title(
+    'YouTube Downloader'
+)
+canvas = Canvas(screen, width=500, height= 500)
+canvas.pack()
